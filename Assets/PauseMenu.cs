bbf52188f018ca2abda9e1d3befe7c0ca2273715
mike <1022986@student.sae.edu.au>
2021-10-01 16:06:16 +0800

    removal of uneeded scripts and setting up the UI buttons interactivity

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -8,33 +9,54 @@ public class PauseMenu : MonoBehaviour
 
     public static bool paused = false;
     public GameObject pauseMenuUI;
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKey(KeyCode.Escape))
-        {
-            if (paused)
-            {
-                Resume();
-            }
-            else
-            {
-                Paused();
-            }
-        }
-    }
+    public GameObject pauseButton;
 
+    // turn the pauseMenuUI off so it is invisible and the player can resume the game, also reseting the timescale
     void Resume()
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1;
         paused = false;
+        pauseButton.SetActive(true);
     }
-
+    // turn the pauseMenuUI on so it is visible and useable to the player, pausing the timescale in the game
     void Paused()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0;
         paused = true;
+        pauseButton.SetActive(false);
+    }
+
+
+    // when the button in game is clicked
+    public void OnClickPause()
+    {
+        // if the game is paused resume, if not pause
+        if (paused)
+        {
+            Resume();
+        }
+        else
+        {
+            Paused();
+        }
+    }
+
+
+
+    public void OnClickLevelSelector()
+    {
+        // SceneManager.LoadLevel(LevelSelector)
+    }
+
+    public void OnClickOptions()
+    {
+        // loads separate UI of options in game
+    }
+
+    public void OnClickMenu()
+    {
+        // SceneManager.loadlevel(MainMenu)
     }
 }
