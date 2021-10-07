7f688b9747ffd36c3a41d406f78957bcc9530762
GreenNectar <nitricgreennectar@gmail.com>
2021-10-07 21:00:59 +0800

    Beautify Part 1
    Fixed Michael's levels intersecting with the setup outline
    Altered the background terrain to be more enjoyable to look at
    Added a bunch of clouds in the scene

--- Assets/Scripts/UI/LevelUnlockController.cs
+++ Assets/Scripts/UI/LevelUnlockController.cs
@@ -14,7 +14,7 @@ namespace DemoDelivery.LevelSelect
 
         private void Start()
         {
-            //SetAsPersistent();
+            SetAsPersistent();
             //ResetLevels();
         }
 
